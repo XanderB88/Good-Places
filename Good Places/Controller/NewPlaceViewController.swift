bdc131762cf25ad9f6289f2326d533d3b2d33032
XanderB88 <alexxanderbykov@gmail.com>
2021-09-30 05:05:26 +0400

    Сreate an action sheet to add an image for a new place.

--- Good Places/Controller/NewPlaceViewController.swift
+++ Good Places/Controller/NewPlaceViewController.swift
@@ -8,10 +8,10 @@
 import UIKit
 
 class NewPlaceViewController: UITableViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //        Delete lines from down of table
         tableView.tableFooterView = UIView()
     }
     
@@ -19,7 +19,24 @@ class NewPlaceViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
+            //            Сreate an actionSheet to add an image for a new place.
+            let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
             
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(source: .camera)
+            }
+            
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.chooseImagePicker(source: .photoLibrary)
+            }
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            
+            present(actionSheet, animated: true, completion: nil)
         } else {
             view.endEditing(true)
         }
@@ -29,12 +46,24 @@ class NewPlaceViewController: UITableViewController {
 // MARK: - Text field delegate
 
 extension NewPlaceViewController: UITextFieldDelegate {
-    
-//    Hide keyboard by pressing done button
-    
+    //    Hide keyboard by pressing done button
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
     
+}
+
+// MARK: - Work with image
+extension NewPlaceViewController {
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
 }
