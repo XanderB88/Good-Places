999933fa3beaed1206c0de24129f79a5acef2515
XanderB88 <alexxanderbykov@gmail.com>
2021-10-04 06:48:16 +0400

    Update Model.

--- Good Places/Model/PlacesModel.swift
+++ Good Places/Model/PlacesModel.swift
@@ -5,27 +5,23 @@
 //  Created by Alexander on 29.09.2021.
 //
 
-import UIKit
+import RealmSwift
 
-struct Place {
+class Place: Object {
     
-    var name: String
-    var location: String?
-    var type: String?
-    var image: UIImage?
-    var placeImage: String?
+    @objc dynamic var name = ""
+    @objc dynamic var location: String?
+    @objc dynamic var type: String?
+    @objc dynamic var image: UIImage?
     
-    static let goodPlacesName = [
+   let goodPlacesName = [
         "Burger Heroes", "Kitchen", "Bonsai", "Дастархан", "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes", "Speak Easy", "Morris Pub", "Вкусные истории", "Классик", "Love&Life", "Шок", "Бочка"
     ]
     
-    static func getPlaces() -> [Place] {
-        var places = [Place]()
-        
+    func savePlaces(){
         for place in goodPlacesName {
-            places.append(Place(name: place, location: "Ufa", type: "Restaurant", image: nil, placeImage: place))
+           let newPlace = Place()
+           newPlace.name = place
         }
-        
-        return places
     }
 }
