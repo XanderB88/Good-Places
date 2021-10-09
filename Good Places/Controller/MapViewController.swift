6d94fa53c1c5069bf1b81571d4aa86b4cc009423
XanderB88 <alexxanderbykov@gmail.com>
2021-10-10 03:52:20 +0400

    Customise the annotation of place.

--- Good Places/Controller/MapViewController.swift
+++ Good Places/Controller/MapViewController.swift
@@ -10,11 +10,13 @@ import MapKit
 
 class MapViewController: UIViewController {
     var place: Place!
+    let annotationIdentifier = "annotationIdentifier"
     
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
         setupPlacemark()
     }
     
@@ -46,5 +48,29 @@ class MapViewController: UIViewController {
     
     @IBAction func cancelPressed(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
+    }
+}
+
+// MARK: - Map view delegate
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard !(annotation is MKUserLocation) else { return nil }
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier)
+       
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+            annotationView?.canShowCallout = true
+        }
+        
+        if let imageData = place.imageData {
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+            imageView.layer.cornerRadius = 10
+            imageView.clipsToBounds = true
+            imageView.image = UIImage(data: imageData)
+            annotationView?.rightCalloutAccessoryView = imageView
+        }
+        
+        return annotationView
     }
 }
