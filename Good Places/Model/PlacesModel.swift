c716670bb706a70dee430a5613402d8415421de7
XanderB88 <alexxanderbykov@gmail.com>
2021-10-09 04:03:57 +0400

    Save the rating of place to DB.

--- Good Places/Model/PlacesModel.swift
+++ Good Places/Model/PlacesModel.swift
@@ -13,12 +13,18 @@ class Place: Object {
     @Persisted var type: String?
     @Persisted var imageData: Data?
     @Persisted var date = Date()
-
-    convenience init(name: String, location: String?, type: String?, imageData: Data?) {
+    @Persisted var rating = 0.0
+    
+    convenience init(name: String,
+                     location: String?,
+                     type: String?,
+                     imageData: Data?,
+                     rating: Double) {
         self.init()
         self.name = name
         self.location = location
         self.type = type
         self.imageData = imageData
+        self.rating = rating
     }
 }
