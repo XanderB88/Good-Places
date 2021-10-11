e7f2060e0bf70ffb02c9f9636c35118e80371efa
XanderB88 <alexxanderbykov@gmail.com>
2021-10-11 06:25:33 +0400

    Add the user location on the map.

--- Good Places/Controller/MapViewController.swift
+++ Good Places/Controller/MapViewController.swift
@@ -7,10 +7,13 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
     var place = Place()
     let annotationIdentifier = "annotationIdentifier"
+    let locationManager = CLLocationManager()
+    let regionInMeters = 1_000.0
     
     @IBOutlet weak var mapView: MKMapView!
     
@@ -18,6 +21,7 @@ class MapViewController: UIViewController {
         super.viewDidLoad()
         mapView.delegate = self
         setupPlacemark()
+        checkLocationServices()
     }
     
     private func setupPlacemark() {
@@ -46,8 +50,31 @@ class MapViewController: UIViewController {
         }
     }
     
+    private func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+        } else {
+            
+        }
+    }
+    
+    private func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
     @IBAction func cancelPressed(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func centerUserLocation(_ sender: UIButton) {
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion(center: location,
+                                            latitudinalMeters: regionInMeters,
+                                            longitudinalMeters: regionInMeters)
+            
+            mapView.setRegion(region, animated: true)
+        }
     }
 }
 
@@ -57,7 +84,7 @@ extension MapViewController: MKMapViewDelegate {
         guard !(annotation is MKUserLocation) else { return nil }
         
         var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier)
-       
+        
         if annotationView == nil {
             annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
             annotationView?.canShowCallout = true
@@ -72,5 +99,35 @@ extension MapViewController: MKMapViewDelegate {
         }
         
         return annotationView
+    }
+}
+
+// MARK: - Location manager delegate
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+            case .notDetermined:
+                manager.requestWhenInUseAuthorization()
+                break
+            case .authorizedWhenInUse:
+                mapView.showsUserLocation = true
+                break
+            case .denied, .restricted:
+                let alert = UIAlertController(title: "Your location is not avalible", message:"For using your location to give permission go to settings" , preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+                alert.addAction(UIAlertAction(title: "Settings", style: .default, handler: { _ in
+                    let url = URL(string: "App-prefs:root=LOCATION_SERVICES")!
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                }))
+               
+                self.present(alert, animated: true, completion: nil)
+               
+                break
+            case .authorizedAlways:
+                break
+            @unknown default:
+                print("New case is avalible")
+        }
     }
 }
